cf5372a8addbe15df9c2e1ce1bef364da7b76164
Andrey <andrey.sidorovya@ya.ru>
2020-02-24 18:14:12 +0300

    feat: add solution.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) 
+{
+  if (array == null || array.length == 0)
+    return 0;
+
+  let min = Number.MAX_SAFE_INTEGER;
+
+  for (let element of array)
+  {
+    if (element < min)
+      min = element;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == null || array.length == 0)
+    return 0;
+
+  let max = Number.MIN_SAFE_INTEGER;
+
+  for (let element of array)
+  {
+    if (element > max)
+      max = element;
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == null || array.length == 0)
+    return 0;
+
+  let average = 0;
+  for (let element of array)
+  {
+      average += element;
+  }
+
+  return average / array.length;
 }
